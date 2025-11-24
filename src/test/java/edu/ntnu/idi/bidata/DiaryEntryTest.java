d21c60abedd57f994c56315fa5456552743a9ddf
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-11-24 23:27:04 +0100

    Updated test class for DiaryEntry

--- src/test/java/edu/ntnu/idi/bidata/DiaryEntryTest.java
+++ src/test/java/edu/ntnu/idi/bidata/DiaryEntryTest.java
@@ -9,19 +9,24 @@ import org.junit.jupiter.api.Test;
 public class DiaryEntryTest {
 
   private LocalDateTime testTime;
+  private DiaryRegistry diaryRegistry;
+  private AuthorRegistry authorRegistry;
 
   @BeforeEach
   void setUp() {
+    diaryRegistry = new DiaryRegistry();
+    authorRegistry = new AuthorRegistry();
     testTime = LocalDateTime.of(2024, 10, 24, 16, 30);
   }
 
   @Test
   void testCreateValidDiaryEntry() {
-    DiaryEntry entry = new DiaryEntry(1, "Ola Nordmann", testTime, "Test day",
+    Author author1 = authorRegistry.createAndAddAuthor("Ola Nordmann");
+    DiaryEntry entry = new DiaryEntry(1, author1, testTime, "Test day",
         "Today i tested class DiaryEntry", "Tests");
 
     assertEquals(1, entry.getId());
-    assertEquals("Ola Syrstad Berg", entry.getAuthor());
+    assertEquals("Ola Nordmann", entry.getAuthor());
     assertEquals(testTime, entry.getTimestamp());
     assertEquals("Test day", entry.getTitle());
     assertEquals("Today i tested class DiaryEntry", entry.getContent());
