ef787a69f0f69337cf1807972f3cfe7c32df5a15
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-11-24 22:31:25 +0100

    Updated application class with start() and init()

--- src/main/java/edu/ntnu/idi/bidata/Application.java
+++ src/main/java/edu/ntnu/idi/bidata/Application.java
@@ -1,8 +1,42 @@
 package edu.ntnu.idi.bidata;
 
+/**
+ * <h1>Application.</h1>
+ *
+ * <p>This class represents the main application and user interface for the diary system.
+ * Initializes the system and handles the functionality of the system.</p>
+ */
 public class Application {
-  
+
+  private DiaryRegistry diaryRegistry;
+  private AuthorRegistry authorRegistry;
+
+  /**
+   * The main entry point of the application.
+   *
+   * @param args The command line arguments.
+   */
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    Application app = new Application();
+
+    app.init();
+    app.start();
+  }
+
+  /**
+   * Initializes the application by creating instances of the registry classes.
+   */
+  public void init() {
+    System.out.println("INITIALIZING DIARY APPLICATION");
+
+    diaryRegistry = new DiaryRegistry();
+    authorRegistry = new AuthorRegistry();
+  }
+
+  /**
+   * Starts the application and runs functionality.
+   */
+  public void start() {
+    System.out.println("STARTING DIARY APPLICATION");
   }
 }
