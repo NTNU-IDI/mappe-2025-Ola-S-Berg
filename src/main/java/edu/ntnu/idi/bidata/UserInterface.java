b8440a3305e637a335f892fd80136721bb1da471
Ola Syrstad Berg <olasyrstadberg@yahoo.com>
2025-11-27 11:19:13 +0100

    Created intial methods for the UI class

--- src/main/java/edu/ntnu/idi/bidata/UserInterface.java
+++ src/main/java/edu/ntnu/idi/bidata/UserInterface.java
@@ -1,5 +1,45 @@
 package edu.ntnu.idi.bidata;
 
+import java.util.Scanner;
+
+/**
+ * <h1>User Interface.</h1>
+ *
+ * <p>Handles all user interaction for the diary application using a text-based menu system.</p>
+ */
 public class UserInterface {
 
+  private DiaryRegistry diaryRegistry;
+  private AuthorRegistry authorRegistry;
+  private Scanner scanner;
+  private boolean running;
+
+  /**
+   * Initializes the user interface.
+   */
+  public void init() {
+    diaryRegistry = new DiaryRegistry();
+    authorRegistry = new AuthorRegistry();
+    scanner = new Scanner(System.in);
+    running = true;
+  }
+
+  /**
+   * Starts the application.
+   */
+  public void start() {
+
+    while (running) {
+      displayMenu();
+    }
+
+    scanner.close();
+  }
+
+  /**
+   * Displays the menu.
+   */
+  private void displayMenu() {
+    System.out.println("Main menu");
+  }
 }
